7a02b5e4c80b5513ecabf7937dd2bd92f420bd6f
hoonlocal <hoonlocal@naver.com>
2021-07-04 01:13:10 +0900

    webpack issue solve _ vue-loader version problem

---
+++ 3.숫자야구/main.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import NumberBaseball from './NumberBaseball.vue';
+
+new Vue().$mount('#root');
--- 3.숫자야구/webpack.config.js
+++ 3.숫자야구/webpack.config.js
@@ -1,13 +1,22 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const path = require('path');
+
 module.exports = {
     entry: {
-        app: './main.js',
+        // app: './main.js',
+        app: path.join(__dirname, 'main.js'),
     },
     module: {
-
+        rules: [{
+            test: /\.vue$/,
+            loader: 'vue-loader',
+        }],
     },
-    plugins: [],
+    plugins: [
+        new VueLoaderPlugin(),
+    ],
     output: {
         filename: 'app.js',
-        path: './dist',
+        path: path.join(__dirname, 'dist'),
     },
 };
